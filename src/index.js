fe10f8efddbe6c703350418fcd5f4a3d03dd09c3
Vaibhav <vaibhavsathawane11@gmail.com>
2021-09-29 23:53:45 +0530

    routr

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
